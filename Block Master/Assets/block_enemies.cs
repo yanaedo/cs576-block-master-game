699fb8f1087d18e337fc256585ef05e4e3075f5e
grievans <gevans@umass.edu>
2022-12-10 22:21:26 -0500

    Setting up initial test area

---
+++ Block Master/Assets/block_enemies.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class block_enemies : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO
+        // Collider playerCollider = GetComponent<Collider>(); //TODO finish setting this up; need a player object type I think?
+        // Collider[] keyColliders = ............ //TODO
+        // Physics.IgnoreCollision(playerCollider, GetComponent<Collider>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

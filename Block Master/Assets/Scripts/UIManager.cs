e58a7054098bea316a1b36a022b40323509d25d1
Tatyana <tatyanaedozie@gmail.com>
2022-12-14 16:49:28 -0500

    updated settings menu

--- Block Master/Assets/Scripts/UIManager.cs
+++ Block Master/Assets/Scripts/UIManager.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ToggleDialogs : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
     private GameObject instructionsDialog;
+    private GameObject settingsDialog;
 
     // Start is called before the first frame update
     void Start()
     {
         instructionsDialog = GameObject.Find("InstructionsContainer");
         instructionsDialog.SetActive(false);
+        settingsDialog = GameObject.Find("SettingsContainer");
+        settingsDialog.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,5 +25,16 @@ public class ToggleDialogs : MonoBehaviour
     public void showDialog(GameObject dialog)
     {
         dialog.SetActive(!dialog.activeSelf);
+    }
+
+    public void setVolume(float volume)
+    {
+
+    }
+
+    public void exitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
     }
 }

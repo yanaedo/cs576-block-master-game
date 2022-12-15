ab9dd01b5754fdeeac9df9d1623270b6e9524a5d
Tatyana <tatyanaedozie@gmail.com>
2022-12-14 22:07:31 -0500

    made changes to settings

--- Block Master/Assets/Scripts/UIManager.cs
+++ Block Master/Assets/Scripts/UIManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class UIManager : MonoBehaviour
 {
+    public AudioMixer audioMix;
+    public GameObject fullscreenToggle;
     private GameObject instructionsDialog;
     private GameObject settingsDialog;
 
@@ -14,6 +19,12 @@ public class UIManager : MonoBehaviour
         instructionsDialog.SetActive(false);
         settingsDialog = GameObject.Find("SettingsContainer");
         settingsDialog.SetActive(false);
+        UnityEngine.UI.Toggle toggle = fullscreenToggle.GetComponent<UnityEngine.UI.Toggle>();
+
+        ColorBlock colors = toggle.colors;
+            colors.normalColor = new Color(255, 174, 47, 1);
+            toggle.colors = colors;
+       
     }
 
     // Update is called once per frame
@@ -27,9 +38,40 @@ public class UIManager : MonoBehaviour
         dialog.SetActive(!dialog.activeSelf);
     }
 
-    public void setVolume(float volume)
+    public void setBackgroundVolume(float volume)
     {
+        audioMix.SetFloat("backgroundMusicVolume", volume);
+    }
 
+    public void setGameSoundsVolume(float volume)
+    {
+        audioMix.SetFloat("gameSoundsVolume", volume);
+    }
+
+    public void setQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void setFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void changeButtonColor()
+    {
+        /*ColorBlock colors = fullscreenToggle.colors;
+
+        if (fullscreenToggle.isOn)
+        {
+            colors.normalColor = new Color(255, 174, 47);
+            fullscreenToggle.colors = colors;
+        }
+        else
+        {
+            colors.normalColor = new Color(255, 255, 255, 0.6f);
+            fullscreenToggle.colors = colors;
+        }*/
     }
 
     public void exitGame()
